f4b401cb92ab581f64e0678b170063e4abd400d6
leonkae <leon29kae@gmail.com>
2022-08-10 13:29:05 +0300

    add:blogpost model

--- jot/models.py
+++ jot/models.py
@@ -7,4 +7,27 @@ class Profile(models.Model):
     prophoto = models.ImageField(upload_to='profile/', default = 'image.jpg')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
     following = models.ManyToManyField(User, related_name ='following')
-    followers = models.ManyToManyField(User, related_name= 'followers')    
+    followers = models.ManyToManyField(User, related_name= 'followers')  
+    
+    def __str__(self):
+        return self.user.username
+    
+    
+
+
+class Blogpost(models.Model):
+    '''blogpost model'''
+    title = models.CharField(max_length=255)
+    blog = models.TextField()
+    image = models.ImageField(upload_to='blogpost/',default='img.jpg')
+    created = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='blog_post')
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    comments = models.ManyToManyField(Comment,blank=True)
+    
+    
+    def __str__(self):
+        return f'{self.profile.user.username} Blogpost'
+    
+        
+    

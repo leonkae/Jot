bfa1b2423f9f19100c59b369c9d179cf3be91f8e
leonkae <leon29kae@gmail.com>
2022-08-10 13:07:49 +0300

    add:Profile MOdels

--- jot/models.py
+++ jot/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    '''profile model'''
+    bio = models.TextField(max_length=255)
+    prophoto = models.ImageField(upload_to='profile/', default = 'image.jpg')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
+    following = models.ManyToManyField(User, related_name ='following')
+    followers = models.ManyToManyField(User, related_name= 'followers')    
